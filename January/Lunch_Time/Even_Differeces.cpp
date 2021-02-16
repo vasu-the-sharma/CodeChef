c338001a6445142a75779776f75ca50f03d8c97d
VASU SHARMA <vasuthesharma@gmail.com>
2021-02-16 17:12:04 +0530

    Create Even_Differeces.cpp

---
+++ January/Lunch_Time/Even_Differeces.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; i++){
+            cin>>arr[i];
+        }
+
+        int count1=0, count2=0;
+        for(int i=0; i<n; i++){
+            if(arr[i]%2==0)
+                count1++;
+            else
+                count2++;
+        }
+
+        cout<<std::min(count1, count2)<<endl;
+    }
+}
