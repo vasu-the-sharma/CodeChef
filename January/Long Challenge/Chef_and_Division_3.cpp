7b140d3e004785917f6aeef1c51c69670705d03f
VASU SHARMA <vasuthesharma@gmail.com>
2021-02-16 17:03:20 +0530

    Create Chef_and_Division_3.cpp

---
+++ January/Long Challenge/Chef_and_Division_3.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n, k, d;
+        cin>>n>>k>>d;
+        
+        int arr[n];
+        for(int i=0; i<n; i++){
+            cin>>arr[i];
+        }
+
+        int sum=0;
+        for(int i=0; i<n; i++){
+            sum+=arr[i];
+        }
+
+        if(sum<k){
+            cout<<0;
+        }else{
+            int ans=sum/k;
+            if(ans<d)
+                cout<<ans;
+            else
+                cout<<d;
+        }
+        cout<<endl;
+    }
+return 0;
+}
