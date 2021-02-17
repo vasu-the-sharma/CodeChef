dca04f8f1446bcdf0234ab39c2cb4823a3b00d9c
VASU SHARMA <vasuthesharma@gmail.com>
2021-02-17 08:48:31 +0530

    Update Maximise_Function.cpp

--- February/Long_Challenge/Maximise_Function.cpp
+++ February/Long_Challenge/Maximise_Function.cpp
@@ -1,1 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int t;
+    cin>>t;
+
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        for(int i=0; i<n; i++){
+            cin>>arr[i];
+        }
+
+        long long int maxi=arr[0], mini=arr[0];
+        for(int i=0; i<n; i++){
+            if(arr[i]>maxi)
+                maxi=arr[i];
+            if(arr[i]<mini)
+                mini=arr[i];
+        }
+
+        long long int ans=maxi-mini;
+        cout<<ans*2<<endl;
+    }
+}
 
