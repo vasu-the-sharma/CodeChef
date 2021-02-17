45da407f57d411b543afc526622e397701e3b80e
VASU SHARMA <vasuthesharma@gmail.com>
2021-02-17 08:48:00 +0530

    Update Highest_Divisor.cpp

--- February/Long_Challenge/Highest_Divisor.cpp
+++ February/Long_Challenge/Highest_Divisor.cpp
@@ -1,1 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+
+    int ans;
+    for(int i=10; i>=1; i--){
+        if(n%i==0){
+            ans=i;
+            break;
+        }
+    }
+    cout<<ans;
+}
 
